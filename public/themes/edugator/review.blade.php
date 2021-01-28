eabbf4030a81ecbd3a1b8cb3f9a7d59af4709ab3
peeraphut <p.peeraphut@gmail.com>
2021-01-28 15:30:08 +0700

    new

--- public/themes/edugator/review.blade.php
+++ public/themes/edugator/review.blade.php
@@ -8,7 +8,6 @@
         <div class="row">
             <div class="col-md-8 offset-md-2">
 
-                
                 <div class="review-view-wrap my-5">
 
                     <div class="card">
